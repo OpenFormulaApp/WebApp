a341c1c5f7feb868181edaece03b1ce7ce229e1a
Uri Mtz <39018208+UriMtzF@users.noreply.github.com>
2023-04-04 14:10:24 -0600

    Creado footer con custom-element
    Se modificó custom-elements para importar bootstrap

--- js/custom-elements.js
+++ js/custom-elements.js
@@ -1,29 +1,53 @@
-function addActiveClass() {
-  const links = this.querySelectorAll('.nav-link');
-  const currentPath = window.location.pathname;
+window.addEventListener('load',() =>{
+  const bootstrapStyles = document.createElement('link');
+  bootstrapStyles.rel = 'stylesheet';
+  bootstrapStyles.href = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css';
+  document.head.appendChild(bootstrapStyles);
 
-  links.forEach(link => {
-    if (link.getAttribute('href') === currentPath) {
-      link.classList.add('active');
+  const bootstrapScript = document.createElement('script');
+  bootstrapScript.src = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js';
+  document.body.appendChild(bootstrapScript);
+
+  function addActiveClass() {
+    const links = this.querySelectorAll('.nav-link');
+    const currentPath = window.location.pathname;
+
+    links.forEach(link => {
+      if (link.getAttribute('href') === currentPath) {
+        link.classList.add('active');
+      }
+    });
+  }
+
+  // Navbar custom-element
+  class Navbar extends HTMLElement{
+    constructor(){
+      super();
     }
-  });
-}
-
-
-
-// Navbar custom-element
-class Navbar extends HTMLElement{
-  constructor(){
-    super();
+    connectedCallback(){
+      fetch('templates/navbar.html')
+      .then(response => response.text())
+      .then(data =>{
+        this.innerHTML = data;
+        this.addActiveClass = addActiveClass.bind(this);
+        this.addActiveClass();
+      })
+    }
   }
-  connectedCallback(){
-    fetch('templates/navbar.html')
-    .then(response => response.text())
-    .then(data =>{
-      this.innerHTML = data;
-      this.addActiveClass = addActiveClass.bind(this);
-      this.addActiveClass();
-    })
+  customElements.define('of-navbar',Navbar);
+
+  //Footer custom-element
+  class Footer extends HTMLElement{
+    constructor(){
+      super();
+    }
+    connectedCallback(){
+      fetch('templates/footer.html')
+      .then(response => response.text())
+      .then(data => {
+        this.innerHTML = data;
+      })
+    }
   }
-}
-customElements.define('of-navbar',Navbar);
+  customElements.define('of-footer',Footer);
+})
