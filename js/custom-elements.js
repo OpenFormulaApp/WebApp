221c753b198630ff8918a338fad6a7492200fed6
Uri Mtz <39018208+UriMtzF@users.noreply.github.com>
2023-04-09 17:36:45 -0600

    Aplicadas reglas por defecto de Prettier

--- js/custom-elements.js
+++ js/custom-elements.js
@@ -1,67 +1,71 @@
-window.addEventListener('load',() =>{
-  const bootstrapStyles = document.createElement('link');
-  bootstrapStyles.rel = 'stylesheet';
-  bootstrapStyles.href = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css';
+window.addEventListener("load", () => {
+  const bootstrapStyles = document.createElement("link");
+  bootstrapStyles.rel = "stylesheet";
+  bootstrapStyles.href =
+    "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css";
   document.head.appendChild(bootstrapStyles);
 
-  const bootstrapScript = document.createElement('script');
-  bootstrapScript.src = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js';
+  const bootstrapScript = document.createElement("script");
+  bootstrapScript.src =
+    "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js";
   document.body.appendChild(bootstrapScript);
 
   function addActiveClass() {
-    const links = this.querySelectorAll('.nav-link');
+    const links = this.querySelectorAll(".nav-link");
     const currentPath = window.location.pathname;
 
-    links.forEach(link => {
-      if (link.getAttribute('href') === currentPath) {
-        link.classList.add('active');
+    links.forEach((link) => {
+      if (link.getAttribute("href") === currentPath) {
+        link.classList.add("active");
       }
     });
   }
 
   // Navbar custom-element
-  class Navbar extends HTMLElement{
-    constructor(){
+  class Navbar extends HTMLElement {
+    constructor() {
       super();
     }
-    connectedCallback(){
-      fetch('templates/navbar.html')
-      .then(response => response.text())
-      .then(data =>{
-        this.innerHTML = data;
-        this.addActiveClass = addActiveClass.bind(this);
-        this.addActiveClass();
-      })
+    connectedCallback() {
+      fetch("templates/navbar.html")
+        .then((response) => response.text())
+        .then((data) => {
+          this.innerHTML = data;
+          this.addActiveClass = addActiveClass.bind(this);
+          this.addActiveClass();
+        });
     }
   }
-  customElements.define('of-navbar',Navbar);
+  customElements.define("of-navbar", Navbar);
 
   //Footer custom-element
-  class Footer extends HTMLElement{
-    constructor(){
+  class Footer extends HTMLElement {
+    constructor() {
       super();
     }
-    connectedCallback(){
-      fetch('templates/footer.html')
-      .then(response => response.text())
-      .then(data => {
-        this.innerHTML = data;
-      })
+    connectedCallback() {
+      fetch("templates/footer.html")
+        .then((response) => response.text())
+        .then((data) => {
+          this.innerHTML = data;
+        });
     }
   }
-  customElements.define('of-footer',Footer);
+  customElements.define("of-footer", Footer);
 
-  class breadcrumb extends HTMLElement{
-    constructor(){
+  class breadcrumb extends HTMLElement {
+    constructor() {
       super();
     }
 
-    connectedCallback(){
+    connectedCallback() {
       let rutaActual = window.location.pathname.split("/");
       //rutaActual = rutaActual.shift();
       let ultimoElemento = rutaActual.pop();
-      ultimoElemento = ultimoElemento.charAt(0).toUpperCase() + ultimoElemento.slice(1).slice(0,-5);
-      this.ruta = rutaActual.filter((ruta) => ruta !== '');
+      ultimoElemento =
+        ultimoElemento.charAt(0).toUpperCase() +
+        ultimoElemento.slice(1).slice(0, -5);
+      this.ruta = rutaActual.filter((ruta) => ruta !== "");
 
       let html = '<nav aria-label="breadcrumb"><ol class="breadcrumb">';
       html += '<li class="breadcrumb-item"><a href="/">Inicio</a></li>';
@@ -71,10 +75,10 @@ window.addEventListener('load',() =>{
       });
 
       html += `<li class="breadcrumb-item active" aria-current="page">${ultimoElemento}</li>`;
-      html += '</ol></nav>';
+      html += "</ol></nav>";
 
       this.innerHTML = html;
     }
   }
-  customElements.define('of-breadcrumb',breadcrumb);
-})
+  customElements.define("of-breadcrumb", breadcrumb);
+});
--- js/tableConfig.js
+++ js/tableConfig.js
@@ -1,53 +1,61 @@
-const bootstrapStyle = document.createElement('link');
-bootstrapStyle.rel = 'stylesheet';
-bootstrapStyle.href = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css';
+const bootstrapStyle = document.createElement("link");
+bootstrapStyle.rel = "stylesheet";
+bootstrapStyle.href =
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css";
 document.head.appendChild(bootstrapStyle);
 
-const katexStyle = document.createElement('link');
-katexStyle.rel = 'stylesheet';
-katexStyle.href = 'https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.css';
+const katexStyle = document.createElement("link");
+katexStyle.rel = "stylesheet";
+katexStyle.href =
+  "https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.css";
 document.head.appendChild(katexStyle);
 
-const dataTablesStyle = document.createElement('link');
-dataTablesStyle.rel = 'stylesheet';
-dataTablesStyle.href = 'https://cdn.datatables.net/v/bs5/dt-1.13.4/sb-1.4.2/sp-2.1.2/datatables.min.css';
+const dataTablesStyle = document.createElement("link");
+dataTablesStyle.rel = "stylesheet";
+dataTablesStyle.href =
+  "https://cdn.datatables.net/v/bs5/dt-1.13.4/sb-1.4.2/sp-2.1.2/datatables.min.css";
 document.head.appendChild(dataTablesStyle);
 
-const jQueryScript = document.createElement('script');
-jQueryScript.src = 'https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.min.js';
+const jQueryScript = document.createElement("script");
+jQueryScript.src =
+  "https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.min.js";
 jQueryScript.onload = () => {
-  const dataTablesScript = document.createElement('script');
-  dataTablesScript.src = 'https://cdn.datatables.net/v/bs5/dt-1.13.4/sb-1.4.2/sp-2.1.2/datatables.min.js';
-  dataTablesScript.onload = () =>{
-    const bootstrapScript = document.createElement('script');
-    bootstrapScript.src = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js';
+  const dataTablesScript = document.createElement("script");
+  dataTablesScript.src =
+    "https://cdn.datatables.net/v/bs5/dt-1.13.4/sb-1.4.2/sp-2.1.2/datatables.min.js";
+  dataTablesScript.onload = () => {
+    const bootstrapScript = document.createElement("script");
+    bootstrapScript.src =
+      "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js";
     document.body.appendChild(bootstrapScript);
 
-    const dataTablesSearch = document.createElement('script');
-    dataTablesSearch.src = 'https://cdn.datatables.net/plug-ins/1.13.4/features/fuzzySearch/dataTables.fuzzySearch.js';
+    const dataTablesSearch = document.createElement("script");
+    dataTablesSearch.src =
+      "https://cdn.datatables.net/plug-ins/1.13.4/features/fuzzySearch/dataTables.fuzzySearch.js";
     document.body.appendChild(dataTablesSearch);
 
-    const katexScript = document.createElement('script');
-    katexScript.src = 'https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.js';
+    const katexScript = document.createElement("script");
+    katexScript.src =
+      "https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.js";
     document.body.appendChild(katexScript);
 
     function renderKatex(data, type, row) {
-        if (type === "display") {
-          return katex.renderToString(data);
-        }
-        return data;
+      if (type === "display") {
+        return katex.renderToString(data);
       }
-    function showModal(informacion){
+      return data;
+    }
+    function showModal(informacion) {
       informacion = informacion.replace(/(?<!\$)\n(?!.*?\$)/g, "<br>");
 
-      informacion = informacion.replace(/\$\$(.*?)\$\$/g, function(match,p1){
-        try{
-          return katex.renderToString(p1,{
+      informacion = informacion.replace(/\$\$(.*?)\$\$/g, function (match, p1) {
+        try {
+          return katex.renderToString(p1, {
             throwOnError: false,
             displayMode: false,
             fontSize: 14,
           });
-        } catch (error){
+        } catch (error) {
           console.error(error);
           return match;
         }
@@ -68,41 +76,49 @@ jQueryScript.onload = () => {
             </div>
         </div>
       `);
-      
-      $('body').append(modal);
-      modal.modal('show');
+
+      $("body").append(modal);
+      modal.modal("show");
     }
-      let table = new DataTable("#formulas", {
-        ajax: {
-          url: remoteJSON,
-          dataSrc: "",
+    let table = new DataTable("#formulas", {
+      ajax: {
+        url: remoteJSON,
+        dataSrc: "",
+      },
+      columns: [
+        {
+          data: "nombre",
+          createdCell: function (td, cellData, rowData, row, col) {
+            $(td).on("click", function () {
+              showModal(rowData.informacion);
+            });
+          },
         },
-        columns: [
-          { data: "nombre", createdCell: function (td, cellData, rowData, row, col){
-            $(td).on("click", function(){
-              showModal(rowData.informacion)
-            })
-          } },
-          { data: "formula", orderable: false, render: renderKatex },
-        ],
-        columnDefs:[
-          {
-            targets: 1,
-            className: 'dt-center'
-          },{
-            targets: 0,
-            render: function(data, type, row){
-              return '<span class="badge text-bg-info rounded-pill"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle-fill" viewBox="0 0 16 16"><path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/></svg></i></span>' + ' ' +data;
-            }
-          }
-        ],
-        fuzzySearch: true,
-        order: [[0, "des"]],
-        language: {
-          url: '//cdn.datatables.net/plug-ins/1.13.4/i18n/es-MX.json'
-        }
-      });
-  }
+        { data: "formula", orderable: false, render: renderKatex },
+      ],
+      columnDefs: [
+        {
+          targets: 1,
+          className: "dt-center",
+        },
+        {
+          targets: 0,
+          render: function (data, type, row) {
+            return (
+              '<span class="badge text-bg-info rounded-pill"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle-fill" viewBox="0 0 16 16"><path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/></svg></i></span>' +
+              " " +
+              data
+            );
+          },
+        },
+      ],
+      fuzzySearch: true,
+      order: [[0, "des"]],
+      language: {
+        url: "//cdn.datatables.net/plug-ins/1.13.4/i18n/es-MX.json",
+      },
+    });
+  };
   document.body.appendChild(dataTablesScript);
-}
+};
 document.body.appendChild(jQueryScript);
